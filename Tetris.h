df292052e7965a7ed8d40c8d2a36359c67b00f7b
Rainbow02@02 <lanaioffe@mail.ru>
2022-07-14 20:14:17 +0300

    changed size

--- Tetris.h
+++ Tetris.h
@@ -11,19 +11,16 @@ class Tetris
 {
 	protected:
 		WINDOW *w;
-		const int winWidth = 50;
-		const int winHeight = 50;
+		const int winWidth = 42;
+		const int winHeight = 42;
 		
 	public:
-		uint64_t glass [53];
+		uint64_t glass [45];
 		
 		Tetris() 
 		{	
-			for(int i=0; i<53; i++) 
-			{
-				if(i >= 49) {glass[i] = uint64_t(-1);}
-				else {glass[i] = 0;}
-			}
+			for(int i=0; i<winWidth+3; i++) glass[i] = 0;
+			for(int i=winHeight-1; i<winHeight+3; i++) glass[i] = uint64_t(-1);
 			w = newwin(winWidth, winHeight, 0, 0);
         	wborder(w, 0, 0, 0, 0, 0, 0, 0, 0);
 			//wborder(w, '|', '|', '-', '-', '1', '2', '3', '4');
